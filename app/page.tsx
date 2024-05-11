38d2ff2734ab9b46ee6875d82c11d7d0c9178f43
Kgaugelo Bopape <bopape9702@gmail.com>
2024-05-11 22:31:20 +0200

    Added user authentication with sign up and sign in pages.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs"
 
 export default function Home() {
   return (
-    <div className="h-screen">
+    <div className="h-screen flex flex-col gap-y-4">
+      <h1>Dashboard</h1>
+      <UserButton afterSignOutUrl="/" />
       Hello Yeah!
     </div>
   );
